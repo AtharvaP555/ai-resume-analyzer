414d01f1b7e7bd18dd891eecddce527712c307e2
Atharva <atharvapuranik255@gmail.com>
2025-08-29 21:33:26 +0530

    Fixed img links

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -31,7 +31,7 @@ const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
                 <div className="space-y-4 cursor-pointer">
                     {file ? (
                         <div className="uploader-selected-file" onClick={(e) => e.stopPropagation()}>
-                            <img src="../../public/images/pdf.png" alt="pdf" className="size-10" />
+                            <img src="/images/pdf.png" alt="pdf" className="size-10" />
                             <div className="flex items-center space-x-3">
                                 <div>
                                     <p className="text-sm text-gray-700 font-medium truncate max-w-xs">
